5f1f6d04997da74362bed5da293127e1591db431
Đào Duy Thái <thaiddhe176315@fpt.edu.vn>
2024-05-24 22:44:35 +0700

    board list finish

--- memoz/app/(dashboard)/_components/board-card/index.tsx
+++ memoz/app/(dashboard)/_components/board-card/index.tsx
@@ -6,6 +6,8 @@ import { Overlay } from "./overlay";
 import { formatDistanceToNow } from "date-fns";
 import { useAuth } from "@clerk/nextjs";
 import { Footer } from "./footer";
+import { Skeleton } from "@/components/ui/skeleton"
+
 
 interface BoardCardProps {
     id: string;
@@ -38,17 +40,25 @@ export const BoardCard = ({
                     <Image src={imageUrl} fill className="object-fit" alt={title} />
                     <Overlay />
                 </div>
-                {/* <Footer
+                <Footer
                     isFavorite={isFavorite}
                     title={title}
                     authorLabel={authorLabel}
                     createdAtLabel={createdAtLabel}
                     onClick={() => { }}
                     disabled={false}
-                /> */}
+                />
 
             </div>
 
         </Link>
+    )
+}
+
+BoardCard.Skeleton = function BoardCardSkeleton() {
+    return (
+        <div className="aspect-[100/127] rounded-lg overflow-hidden">
+            <Skeleton className="h-full w-full"/>
+        </div>
     )
 }
--- memoz/app/(dashboard)/_components/board-list.tsx
+++ memoz/app/(dashboard)/_components/board-list.tsx
@@ -6,6 +6,7 @@ import { EmptyFavorites } from "./empty-favorites";
 import { EmptySearch } from "./empty-search";
 import { EmptyBoards } from "./empty-boards";
 import { BoardCard } from "./board-card";
+import { NewBoardButton } from "./new-board-button";
 
 interface BoardListProps {
     orgId: string;
@@ -24,8 +25,14 @@ export const BoardList = ({
     if (data === undefined) {
         return (
             <div>
-                Loading...
-            </div>
+                <h2 className="text-3xl">
+                    {query.favorites ? "Favorites boards" : "Team Boards"}
+                </h2>
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
+                    <NewBoardButton orgId={orgId} disabled/>
+                    <BoardCard.Skeleton />
+                </div>
+            </div >
         )
     }
 
@@ -53,6 +60,7 @@ export const BoardList = ({
                 {query.favorites ? "Favorites boards" : "Team Boards"}
             </h2>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
+                <NewBoardButton orgId={orgId} />
                 {data?.map((board) => (
                     <BoardCard
                         key={board._id}
