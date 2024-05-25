a39e55917c4263113be7f86359e1940449ac3c82
Đào Duy Thái <thaiddhe176315@fpt.edu.vn>
2024-05-25 23:41:19 +0700

    finish actions

--- memoz/components/actions.tsx
+++ memoz/components/actions.tsx
@@ -8,12 +8,13 @@ import {
     DropdownMenuSeparator,
     DropdownMenuItem
 } from "@/components/ui/dropdown-menu"
-import { Link2, Trash2 } from "lucide-react"
+import { Link2, Pencil, Trash2 } from "lucide-react"
 import { toast } from "sonner";
 import { useApiMutation } from "@/hooks/use-api-mutation";
 import { api } from "@/convex/_generated/api";
 import { ConfirmModal } from "@/components/confirm-modal";
 import { Button } from "./ui/button";
+import { useRenameModal } from "@/store/use-rename-modal";
 
 interface ActionsProps {
     children: React.ReactNode;
@@ -30,6 +31,9 @@ export const Actions = ({
     id,
     title
 }: ActionsProps) => {
+
+    const { onOpen } = useRenameModal();
+
     const onCopyLink = () => {
         navigator.clipboard.writeText(`${window.location.origin}/board/${id}`)
             .then(() => { toast.success('Link copied') })
@@ -52,6 +56,12 @@ export const Actions = ({
                 <DropdownMenuItem onClick={onCopyLink} className="p-3 cursor-pointer">
                     <Link2 className="h-4 w-4 mr-2" />
                     Copy board link
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() =>{
+                    onOpen(id, title)
+                }} className="p-3 cursor-pointer">
+                    <Pencil className="h-4 w-4 mr-2" />
+                    Rename
                 </DropdownMenuItem>
                 <ConfirmModal
                     header="Delete board?"
