7a70ffd9f7575e45186ea6b721e7eec11bca26d7
Đào Duy Thái <thaiddhe176315@fpt.edu.vn>
2024-05-25 22:41:25 +0700

    delete board

--- memoz/components/actions.tsx
+++ memoz/components/actions.tsx
@@ -8,9 +8,12 @@ import {
     DropdownMenuSeparator,
     DropdownMenuItem
 } from "@/components/ui/dropdown-menu"
-import { Link2 } from "lucide-react"
+import { Link2, Trash2 } from "lucide-react"
 import { toast } from "sonner";
-
+import { useApiMutation } from "@/hooks/use-api-mutation";
+import { api } from "@/convex/_generated/api";
+import { ConfirmModal } from "@/components/confirm-modal";
+import { Button } from "./ui/button";
 
 interface ActionsProps {
     children: React.ReactNode;
@@ -29,8 +32,16 @@ export const Actions = ({
 }: ActionsProps) => {
     const onCopyLink = () => {
         navigator.clipboard.writeText(`${window.location.origin}/board/${id}`)
-        .then(() => {toast.success('Link copied')})
-        .catch(() => {toast.error('Failed to copy link')})
+            .then(() => { toast.success('Link copied') })
+            .catch(() => { toast.error('Failed to copy link') })
+    }
+
+    const { mutate, pending } = useApiMutation(api.board.remove)
+
+    const onDelete = () => {
+        mutate({ id })
+            .then(() => { toast.success('Board deleted') })
+            .catch(() => { toast.error('Failed to delete board') })
     }
     return (
         <DropdownMenu>
@@ -42,6 +53,18 @@ export const Actions = ({
                     <Link2 className="h-4 w-4 mr-2" />
                     Copy board link
                 </DropdownMenuItem>
+                <ConfirmModal
+                    header="Delete board?"
+                    description="This will delete the board and all of its contents."
+                    disabled={pending}
+                    onConfirm={onDelete}
+                >
+                    <Button variant="ghost" className="p-3 cursor-pointer text-sm w-full justify-start font-normal">
+                        <Trash2 className="h-4 w-4 mr-2" />
+                        Delete
+                    </Button>
+                </ConfirmModal>
+
             </DropdownMenuContent>
         </DropdownMenu>
     )
