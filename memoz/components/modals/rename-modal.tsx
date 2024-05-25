2d3eed6825c6551f8de18a5cd05ea4e994475cf5
Đào Duy Thái <thaiddhe176315@fpt.edu.vn>
2024-05-25 23:26:28 +0700

    add modal porvider

---
+++ memoz/components/modals/rename-modal.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogClose,
+    DialogFooter,
+    DialogTitle
+} from "@/components/ui/dialog"
+import { useRenameModal } from "@/store/use-rename-modal"
+
+
+export const RenameModal = () => {
+    const {
+        isOpen,
+        onClose,
+        initialValues
+    } = useRenameModal();
+
+    return(
+        <Dialog open={isOpen} onOpenChange={onClose}>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>
+                        Edit board title
+                    </DialogTitle>
+                    <DialogDescription>
+                        Enter new title for this board
+                    </DialogDescription>
+                </DialogHeader>
+            </DialogContent>
+           
+        </Dialog>
+    )
+}
--- memoz/convex/board.ts
+++ memoz/convex/board.ts
@@ -27,9 +27,9 @@ export const create = mutation({
             throw new Error("Unauthorized")
         }
 
-        const randomImage = images [ Math.floor(Math.random() * images.length)]
+        const randomImage = images[Math.floor(Math.random() * images.length)]
         console.log(randomImage, "TEST")
-        const board = await ctx.db.insert("boards" , {
+        const board = await ctx.db.insert("boards", {
             title: args.title,
             orgId: args.orgId,
             authorId: identity.subject,
@@ -41,13 +41,36 @@ export const create = mutation({
 })
 
 export const remove = mutation({
-    args:{ id: v.id("boards")},
+    args: { id: v.id("boards") },
     handler: async (ctx, args) => {
         const identity = await ctx.auth.getUserIdentity();
-        if(!identity) {
+        if (!identity) {
             throw new Error("Unauthorized")
         }
         //TODO: check to delete favorite relation too
         await ctx.db.delete(args.id)
+    }
+})
+
+export const update = mutation({
+    args: {
+        id: v.id("boards"),
+        title: v.string(),
+    },
+    handler: async (ctx, args) => {
+        const identity = await ctx.auth.getUserIdentity();
+        if (!identity) {
+            throw new Error("Unauthorized")
+        }
+        const title = args.title.trim();
+
+        if (!title) {
+            throw new Error("Title cannot be empty")
+        }
+        if (title.length > 60) {
+            throw new Error("Title cannot be longer than 60 characters")
+        }
+        const board = await ctx.db.patch(args.id, { title: args.title })
+        return board;
     }
 })
