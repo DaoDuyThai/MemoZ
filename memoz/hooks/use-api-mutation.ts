8856a1ad6318e715d51b6d4722e53d58c2dc5cde
Đào Duy Thái <thaiddhe176315@fpt.edu.vn>
2024-05-28 20:48:21 +0700

    a

--- memoz/hooks/use-api-mutation.ts
+++ memoz/hooks/use-api-mutation.ts
@@ -6,6 +6,7 @@ export const useApiMutation = (mutationFunction: any) => {
     const [pending, setPending] = useState(false);
     const apiMutation = useMutation(mutationFunction);
 
+    
     const mutate = (payload: any) => {
         setPending(true)
         return apiMutation(payload)
