207bacf9605e6f8611bed0e02652ea1ecbbf0203
Đào Duy Thái - HE176315 <daoduythai.business@gmail.com>
2024-05-29 09:51:04 +0700

    hot fix

--- memoz/app/board/[boardId]/_components/participants.tsx
+++ memoz/app/board/[boardId]/_components/participants.tsx
@@ -1,6 +1,6 @@
 export const Participants = () => {
     return (
-        <div className="absolute h-12 top-2 right-2 bg-white rounded-md p-2 flex items-center shadow-muted-foreground">
+        <div className="absolute h-12 top-2 right-2 bg-white rounded-md p-2 flex items-center shadow-md">
             participants
         </div>
     )
