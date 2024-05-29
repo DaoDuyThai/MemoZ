6434f63829dfef80e35cd5a83d060db570cc1c1a
Đào Duy Thái - HE176315 <daoduythai.business@gmail.com>
2024-05-29 09:45:45 +0700

    canvas layout

---
+++ memoz/app/board/[boardId]/_components/participants.tsx
@@ -0,0 +1,7 @@
+export const Participants = () => {
+    return (
+        <div className="absolute h-12 top-2 right-2 bg-white rounded-md p-2 flex items-center shadow-muted-foreground">
+            participants
+        </div>
+    )
+}
