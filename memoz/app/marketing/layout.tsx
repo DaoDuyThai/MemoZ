3af534d18b2eaaa6a902f9f58b386c7286feb2b3
LuanDTH0104 <luanduong14@gmail.com>
2024-05-30 07:30:51 +0700

    Xóa file layout riêng chuẩn bị vào file layout chung

--- memoz/app/marketing/layout.tsx
+++ memoz/app/marketing/layout.tsx
@@ -1,15 +1,28 @@
+import Footer from "./_components/footer";
+import Header from "./_components/header";
+
 interface MarketingLayoutProps {
     children: React.ReactNode;
+    title: string;
+    className: string;
 }
 
-const DashboardLayout = ({
+const MarketingLayout = ({
     children,
+    className,
+    title
 }: MarketingLayoutProps) => {
     return (
-        <>
-            {children}
-        </>
+        <main>
+            
+            <Header />
+            <div className={" bg-[#fcf8f0] space-y-5 p-5"}>
+                {children}
+            </div>
+            <Footer />
+        
+        </main>
     )
 }
 
-export default DashboardLayout;
+export default MarketingLayout;
--- memoz/app/marketing/page.tsx
+++ memoz/app/marketing/page.tsx
@@ -1,9 +1,7 @@
 "use client"
 
-import Card from "@/components/common/card";
-import Header from "@/components/common/header";
+import Card from "@/app/marketing/_components/card";
 import { Button } from "@/components/ui/button";
-import MarketingTemplate from "@/templates/MarketingLayout";
 import {
     Carousel,
     CarouselContent,
@@ -11,6 +9,7 @@ import {
     CarouselNext,
     CarouselPrevious,
 } from "@/components/ui/carousel"
+import MarketingLayout from "./layout";
 
 
 const cardData = [
@@ -44,7 +43,7 @@ const cardData = [
 const MarketingPage = () => {
 
     return (
-        <MarketingTemplate className={" bg-[#fcf8f0] space-y-5 p-5"}>
+        <div className="space-y-5">
             <h1 className="text-center text-6xl mt-10">Think and create <br /> All in one visual workspace</h1>
             <div className="text-center">Go from brainstorming to execution, all in one place. <br />
                 Miro is your team's collaborative online workspace.</div>
@@ -73,28 +72,25 @@ const MarketingPage = () => {
                     <CarouselNext />
                 </Carousel>
             </div>
-            <div className="flex flex-col md:flex-row md:p-10">
+            <div className="flex flex-col md:flex-row md:p-10 items-center">
                 <div className="space-y-5 md:w-1/2">
                     <h1 className="text-start text-3xl md:text-4xl">Connect your entire company in a single, secure platform</h1>
-                    <small className="text-center sm:text-start">With enterprise-grade security at its core, MemoZ helps organizations approach a Zero Trust Security philosophy. We offer advanced security integrations, robust data residency, governance solutions for sensitive data,
-                        and an accessibility program targeting Level AA Success Criteria of WCAG 2.1.</small>
+                    <div className="text-center sm:text-start">With enterprise-grade security at its core, MemoZ helps organizations approach a Zero Trust Security philosophy. We offer advanced security integrations, robust data residency, governance solutions for sensitive data,
+                        and an accessibility program targeting Level AA Success Criteria of WCAG 2.1.</div>
                 </div>
                 <img src="/Image1.png" alt="Image 1" className="md:w-1/2" />
             </div>
-            <div className="flex flex-col md:flex-row md:p-10">
+            <div className="flex flex-col md:flex-row md:p-10 md:items-center">
                 <div className="space-y-5 mb-2 md:w-1/2">
                     <h1 className="text-start text-3xl md:text-4xl mb-2">Integrated with your favorite tools? Of course.</h1>
-                    <small className="text-center sm:text-start">To make work more efficient, Miro connects dynamically with over 130 tools across your stack
-                    Jira, Azure DevOps, Asana, Zoom, Teams, and Slack are just some of them.</small>
+                    <div className="text-sm sm:text-start sm:text-base">To make work more efficient, Miro connects dynamically with over 130 tools across your stack
+                    Jira, Azure DevOps, Asana, Zoom, Teams, and Slack are just some of them.</div>
                     <br/>
                     <Button variant={'outline'}>Discover more</Button>
                 </div>
                 <img src="/Image2.svg" alt="Image 2" className="md:w-1/2" />
             </div>
-
-
-
-        </MarketingTemplate>
+        </div>
     )
 }
 
