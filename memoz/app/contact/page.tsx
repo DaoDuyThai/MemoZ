e352aab6c405cac9d7ae535091a2e16973434eec
phucne123111 <134253765+phucne123111@users.noreply.github.com>
2024-05-30 08:00:51 +0700

    Update page.tsx

--- memoz/app/contact/page.tsx
+++ memoz/app/contact/page.tsx
@@ -39,7 +39,7 @@ const ContactPage = () => {
         }
         return errors;
     };
-    
+
 
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
@@ -51,8 +51,6 @@ const ContactPage = () => {
             setErrors({});
         }
     };
-
-
     return (
         <div className="mx-auto px-4 pb-16 bg-amber-50">
             <div className="flex flex-col container  md:flex-row items-center bg-amber-50 p-6 h-screen">
