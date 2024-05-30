e58daa4b3d1fcce9d728d746888964631faa3cec
phucne123111 <134253765+phucne123111@users.noreply.github.com>
2024-05-30 07:57:12 +0700

    Update page.tsx

--- memoz/app/contact/page.tsx
+++ memoz/app/contact/page.tsx
@@ -47,6 +47,8 @@ const ContactPage = () => {
             setErrors({});
         }
     };
+
+    
     return (
         <div className="mx-auto px-4 pb-16 bg-amber-50">
             <div className="flex flex-col container  md:flex-row items-center bg-amber-50 p-6 h-screen">
