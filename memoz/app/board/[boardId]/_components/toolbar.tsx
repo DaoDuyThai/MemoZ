2eb797b0f9dcc8e3af19caed5153bfbfe030a034
Đào Duy Thái <thaiddhe176315@fpt.edu.vn>
2024-06-04 22:40:03 +0700

    Finish room toolbar icons and ui

--- memoz/app/board/[boardId]/_components/toolbar.tsx
+++ memoz/app/board/[boardId]/_components/toolbar.tsx
@@ -1,28 +1,26 @@
 "use client"
+
+import { Circle, MousePointer2, Pencil, Redo2, Square, StickyNote, Type, Undo2 } from "lucide-react"
+import { ToolButton } from "./tool-button"
+
 export const Toolbar = () => {
     return (
-        <div className="absolute top-[50%] -translate-y[50%] left-2 flex flex-col gap-y-4">
+        <div className="absolute top-[50%] -translate-y-[50%] left-2 flex flex-col gap-y-4">
             <div className="bg-white rounded-md p-1.5 flex gap-y-1 flex-col items-center shadow-md">
-                <div>
-                    Pencil
-                </div>
-                <div>
-                    Square
-                </div>
-                <div>
-                    Circle
-                </div>
-                <div>
-                    Ellipses
-                </div>
+                <ToolButton isActive={false} label="Select" icon={MousePointer2} onClick={() => { }} />
+                <ToolButton isActive={false} label="Text" icon={Type} onClick={() => { }} />
+                <ToolButton isActive={false} label="Stick Note" icon={StickyNote} onClick={() => { }} />
+                <ToolButton isActive={false} label="Rectangle" icon={Square} onClick={() => { }} />
+                <ToolButton isActive={false} label="Ellipsis" icon={Circle} onClick={() => { }} />
+                <ToolButton isActive={false} label="Pen" icon={Pencil} onClick={() => { }} />
+
+
             </div>
+
             <div className="bg-white rounded-md p-1.5 flex flex-col items-center shadow-md">
-                <div>
-                    Undo
-                </div>
-                <div>
-                    Redo
-                </div>
+                <ToolButton isDisabled={false} isActive={false} label="Undo" icon={Undo2} onClick={() => { }} />
+                <ToolButton isDisabled={false} isActive={false} label="Redo" icon={Redo2} onClick={() => { }} />
+
             </div>
         </div>
     )
