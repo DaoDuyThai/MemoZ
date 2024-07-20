f632953ec1f621a241b4797370d8907a569a8efd
Đào Duy Thái <thaiddhe176315@fpt.edu.vn>
2024-07-21 00:01:39 +0700

    fix the search in deployment

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,35 +1,29 @@
-"use client"
+"use client";
 
 import { useOrganization } from "@clerk/nextjs";
 import { EmptyOrg } from "./_components/empty-org";
 import { BoardList } from "./_components/board-list";
+import { useSearchParams } from "next/navigation";
 
-interface DashboardPageProps {
-    searchParams:{
-        search?: string;
-        favorites?: string;
-    }
-}
+const DashboardPage = () => {
 
-const DashboardPage = ({
-    searchParams,
-}: DashboardPageProps) => {
-    const { organization } = useOrganization();
-
-    return (
-        <div className="flex-1 h-[calc(100-80px)] p-6 ">
-            
-            {!organization ? (
-                <EmptyOrg />
-            ) : (
-                <BoardList
-                orgId = {organization.id}
-                query = {searchParams} />
-            )
-            }
-
-        </div>
-    )
-}
+  const { organization } = useOrganization();
+  const searchParams = useSearchParams();
+  const search = searchParams.get("search") || undefined;
+  const favorites = searchParams.get("favorites") || undefined
+  
+  return (
+    <div className="w-full h-[calc(100vh-65px)] flex flex-col p-6 overflow-y-scroll">
+      {!organization ? (
+        <EmptyOrg />
+      ) : (
+        <BoardList
+          orgId={organization.id}
+          query={{search, favorites }}
+        />
+      )}
+    </div>
+  );
+};
 
 export default DashboardPage;
