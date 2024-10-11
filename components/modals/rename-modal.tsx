2b25ecd80591e2af125126644f875dd5cbe883e0
Đào Duy Thái <daoduythai.business@gmail.com>
2024-10-11 08:35:48 +0700

    finish search scenarios and list favorites

--- components/modals/rename-modal.tsx
+++ components/modals/rename-modal.tsx
@@ -72,7 +72,6 @@ export const RenameModal = () => {
                             submit
                         </Button>
                     </DialogFooter>
-
                 </form>
             </DialogContent>
 
